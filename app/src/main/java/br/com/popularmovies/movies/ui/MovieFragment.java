b34e817eb1ae1a3fbbff255c5b85be6158affd0d
André Spadetto Leão <andre.sp.leao@gmail.com>
2019-03-18 21:28:36 -0300

    Adding sort menu.

--- app/src/main/java/br/com/popularmovies/movies/ui/MovieFragment.java
+++ app/src/main/java/br/com/popularmovies/movies/ui/MovieFragment.java
@@ -1,7 +1,9 @@
 package br.com.popularmovies.movies.ui;
 
+import android.app.AlertDialog;
 import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -9,6 +11,9 @@ import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -22,6 +27,7 @@ public class MovieFragment extends Fragment {
 
     private MovieViewModel mViewModel;
     private RecyclerView mMoviesRecyclerView;
+    private Observer<Resource<Movies>> moviesObserver;
 
     public static MovieFragment newInstance() {
         return new MovieFragment();
@@ -30,26 +36,8 @@ public class MovieFragment extends Fragment {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-    }
-
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable final ViewGroup container,
-                             @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_movie, container, false);
-        if (container != null) {
-            mMoviesRecyclerView = view.findViewById(R.id.rv_movies);
-            mMoviesRecyclerView.setLayoutManager(new GridLayoutManager(getContext(), 2, GridLayoutManager.VERTICAL, false));
-        }
-        return view;
-    }
-
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        mViewModel = ViewModelProviders.of(this).get(MovieViewModel.class);
-
-        mViewModel.getMovies().observe(this, new Observer<Resource<Movies>>() {
+        this.setHasOptionsMenu(true);
+        moviesObserver = new Observer<Resource<Movies>>() {
             @Override
             public void onChanged(@Nullable Resource<Movies> moviesResource) {
                 if (moviesResource != null)
@@ -66,8 +54,56 @@ public class MovieFragment extends Fragment {
                             break;
                     }
             }
-        });
-
+        };
     }
 
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable final ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_movie, container, false);
+        if (container != null) {
+            mMoviesRecyclerView = view.findViewById(R.id.rv_movies);
+            mMoviesRecyclerView.setLayoutManager(new GridLayoutManager(getContext(), 2, GridLayoutManager.VERTICAL, false));
+        }
+        mViewModel = ViewModelProviders.of(this).get(MovieViewModel.class);
+        mViewModel.getMovies().observe(getViewLifecycleOwner(), moviesObserver);
+        return view;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.menu_sort, menu);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.m_sort) {
+            CharSequence[] values = {"Popularity", "Top Rated"};
+            final AlertDialog sortDialog = new AlertDialog.Builder(getContext())
+                    .setTitle("Sort By:")
+                    .setSingleChoiceItems(values, 0, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            changeSortOrder(which);
+                            dialog.dismiss();
+                        }
+                    })
+                    .create();
+
+            sortDialog.show();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void changeSortOrder(int item) {
+        switch (item) {
+            case 0:
+                mViewModel.getMoviesSortedBy("popularity.desc");
+                break;
+            case 1:
+                mViewModel.getMoviesSortedBy("vote_average.desc");
+                break;
+        }
+    }
 }
