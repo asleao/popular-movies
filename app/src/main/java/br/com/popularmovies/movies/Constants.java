5ffb7582614af879d973f71d9323d35a610a6ece
Andre Spadetto Leao <andre.sp.leao@gmail.com>
2019-03-27 20:10:20 -0300

    Preparing fragment to recieve the fields.

--- app/src/main/java/br/com/popularmovies/movies/Constants.java
+++ app/src/main/java/br/com/popularmovies/movies/Constants.java
@@ -1,6 +1,13 @@
 package br.com.popularmovies.movies;
 
 public final class Constants {
+    public static final String MOVIE_TITLE = "MOVIE_TITLE";
+    public static final String MOVIE_POSTER = "MOVIE_POSTER";
+    public static final String MOVIE_YEAR = "MOVIE_YEAR";
+    public static final String MOVIE_DURATION = "MOVIE_DURATION";
+    public static final String MOVIE_RATING = "MOVIE_RATING";
+    public static final String MOVIE_OVERVIEW = "MOVIE_OVERVIEW";
+
     public static final int INDEX_FILTER_MOST_POPULAR = 0;
     public static final int INDEX_FILTER_HIGHEST_RATED = 1;
     public static final String TITLE_DIALOG_FILTER = "Sort By:";
