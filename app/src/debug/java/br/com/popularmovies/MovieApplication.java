1afcb58e6a30432e67f7fa4d30499b84de86933b
Andre Spadetto Leao <andre.sp.leao@gmail.com>
2019-09-22 19:23:47 -0300

    Updating gradle dependencies.

--- app/src/debug/java/br/com/popularmovies/MovieApplication.java
+++ app/src/debug/java/br/com/popularmovies/MovieApplication.java
@@ -4,7 +4,7 @@ import android.app.Application;
 
 import com.facebook.stetho.Stetho;
 
-class MovieApplication extends Application {
+public class MovieApplication extends Application {
     public void onCreate() {
         super.onCreate();
         Stetho.initializeWithDefaults(this);
