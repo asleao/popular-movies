f44daeef2b80f4f17500395118b8246f8e15c9fe
Andre Spadetto Leao <andre.sp.leao@gmail.com>
2021-04-10 13:43:16 +0100

    Creating compose themes folder

---
+++ app/src/main/java/br/com/popularmovies/ui/theme/Color.kt
@@ -0,0 +1,27 @@
+package br.com.popularmovies.ui.theme
+
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import androidx.compose.ui.graphics.Color
+
+val grey = Color(0xFF212121)
+val greyLight = Color(0xFF484848)
+val greyDark = Color(0xFF000000)
+val orange = Color(0xFFff9100)
+val orangeLight = Color(0xFFffc246)
+val orangeDark = Color(0xFFC56200)
+val white = Color(0xFFFFFFFF)
