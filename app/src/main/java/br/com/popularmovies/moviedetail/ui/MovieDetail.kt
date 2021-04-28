d56c66c0b8f33f4a49c4f5d1a2623f054d617292
André Spadetto Leão <aleao@blissapplications.com>
2021-04-28 21:19:09 +0100

    Upgrading core dependencies

--- app/src/main/java/br/com/popularmovies/moviedetail/ui/MovieDetail.kt
+++ app/src/main/java/br/com/popularmovies/moviedetail/ui/MovieDetail.kt
@@ -1,15 +1,18 @@
 package br.com.popularmovies.moviedetail.ui
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import br.com.popularmovies.entities.movie.Movie
 import br.com.popularmovies.ui.theme.AppTheme
 import br.com.popularmovies.ui.theme.typography
+import com.google.accompanist.glide.rememberGlidePainter
 import org.joda.time.LocalDate
 import java.math.BigDecimal
 
@@ -21,10 +24,10 @@ fun MovieDetail( movie: Movie) {
             .fillMaxWidth()
             .fillMaxHeight()
     ) {
-        /*Image(
+        Image(
             painter = rememberGlidePainter(
                 request = movie.poster,
-                previewPlaceholder = R.drawable.loading,
+//                previewPlaceholder = R.drawable.loading,
                 fadeIn = true
             ),
             contentDescription = null,
@@ -32,7 +35,7 @@ fun MovieDetail( movie: Movie) {
                 .height(180.dp)
                 .fillMaxWidth(),
             contentScale = ContentScale.Crop
-        )*/
+        )
         Text(movie.originalTitle, style = typography.h6)
         Spacer(modifier = Modifier.height(8.dp))
         Text(
