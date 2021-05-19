f6d5ba9718d10260cdf8fe2330ac0a3e584071af
André Spadetto Leão <aleao@blissapplications.com>
2021-05-19 21:42:45 +0100

    Adding scroll on movieDetailScreen

--- app/src/main/java/br/com/popularmovies/moviedetail/ui/MovieScreen.kt
+++ app/src/main/java/br/com/popularmovies/moviedetail/ui/MovieScreen.kt
@@ -5,6 +5,8 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -23,6 +25,7 @@ fun MovieScreen(viewModel: MovieDetailViewModel) {
         modifier = Modifier
             .fillMaxSize()
             .background(color = MaterialTheme.colors.background)
+            .verticalScroll(rememberScrollState())
     ) {
         movie?.let {
             MovieDetail(it)
