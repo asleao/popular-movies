e2682153512f035c8e13a808c99f47855bbc62a8
André Spadetto Leão <aleao@blissapplications.com>
2021-06-25 12:28:52 +0100

    Align trailer column center

--- app/src/main/java/br/com/popularmovies/moviedetail/trailers/ui/MovieTrailers.kt
+++ app/src/main/java/br/com/popularmovies/moviedetail/trailers/ui/MovieTrailers.kt
@@ -2,17 +2,13 @@ package br.com.popularmovies.moviedetail.trailers.ui
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -30,9 +26,12 @@ fun MovieTrailerCard(movieTrailer: MovieTrailer, onClick: () -> Unit) {
         elevation = 2.dp,
         modifier = Modifier
             .width(200.dp)
+            .height(150.dp)
             .clickable(onClick = onClick)
     ) {
-        Column {
+        Column(
+            verticalArrangement = Arrangement.Center
+        ) {
             Image(
                 painter = painterResource(R.drawable.ic_play_circle_outline),
                 contentDescription = null,
