f8a7028a68a1f30b846d5bc22d58d5779960236a
Andre Spadetto Leão <aleao@blissapplications.com>
2021-07-17 18:00:10 +0100

    Update dependencies from di
    Only tests#android#core was kept the same

--- datasource_remote/src/main/java/br/com/popularmovies/datasourceremote/interceptors/AuthorizationInterceptor.kt
+++ datasource_remote/src/main/java/br/com/popularmovies/datasourceremote/interceptors/AuthorizationInterceptor.kt
@@ -11,7 +11,7 @@ class AuthorizationInterceptor : Interceptor {
     @Throws(IOException::class)
     override fun intercept(chain: Interceptor.Chain): Response {
         val original = chain.request()
-        val originalHttpUrl = original.url()
+        val originalHttpUrl = original.url
 
         val url = originalHttpUrl.newBuilder()
                 .addQueryParameter(API_KEY, BuildConfig.MdbApiKey)
