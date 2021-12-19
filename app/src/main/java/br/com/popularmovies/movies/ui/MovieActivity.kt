dbcb58246d429b4ff5ca1f721d83e7e08fd665d8
Andre Spadetto Leão <aleao@blissapplications.com>
2021-12-19 14:09:38 +0000

    Removing ActionBar theme and logic

--- app/src/main/java/br/com/popularmovies/movies/ui/MovieActivity.kt
+++ app/src/main/java/br/com/popularmovies/movies/ui/MovieActivity.kt
@@ -3,39 +3,20 @@ package br.com.popularmovies.movies.ui
 import android.os.Bundle
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.onNavDestinationSelected
-import androidx.navigation.ui.setupActionBarWithNavController
 import br.com.popularmovies.MovieApplication
 import br.com.popularmovies.R
 
 
 class MovieActivity : AppCompatActivity() {
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
-
     override fun onCreate(savedInstanceState: Bundle?) {
         (application as MovieApplication).appComponent.inject(this)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_movie)
-        val host: NavHostFragment = supportFragmentManager
-                .findFragmentById(R.id.fg_movie_navhost) as NavHostFragment?
-                ?: return
-
-        val navController = host.navController
-
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBar(navController, appBarConfiguration)
-    }
-
-    private fun setupActionBar(
-            navController: NavController,
-            appBarConfig: AppBarConfiguration
-    ) {
-        setupActionBarWithNavController(navController, appBarConfig)
     }
 
     override fun onSupportNavigateUp(): Boolean {
--- app/src/main/java/br/com/popularmovies/movies/ui/MovieFragment.kt
+++ app/src/main/java/br/com/popularmovies/movies/ui/MovieFragment.kt
@@ -1,15 +1,15 @@
 package br.com.popularmovies.movies.ui
 
-import android.app.AlertDialog
 import android.content.Context
 import android.os.Bundle
-import android.view.*
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.appcompat.widget.LinearLayoutCompat.HORIZONTAL
 import androidx.core.view.isVisible
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
@@ -19,9 +19,6 @@ import br.com.popularmovies.MovieApplication
 import br.com.popularmovies.R
 import br.com.popularmovies.databinding.FragmentMovieBinding
 import br.com.popularmovies.entities.movie.Movie
-import br.com.popularmovies.entities.movie.MovieOrderType
-import br.com.popularmovies.movies.Constants.*
-import br.com.popularmovies.movies.adapters.MovieAdapter
 import br.com.popularmovies.movies.adapters.MovieClickListener
 import br.com.popularmovies.movies.adapters.MoviePagingAdapter
 import br.com.popularmovies.movies.viewmodel.MovieViewModel
@@ -47,10 +44,7 @@ class MovieFragment : Fragment(), MovieClickListener {
     }
 
     private fun setupObservers() {
-        // setupMoviesObserver()
         setupMoviesPaging()
-//        setupLoadingObserver()
-//        setupErrorObserver()
     }
 
     private fun setupMoviesPaging() {
@@ -73,72 +67,6 @@ class MovieFragment : Fragment(), MovieClickListener {
         }
     }
 
-    private fun setupMoviesObserver() {
-        mViewModel.movies.observe(viewLifecycleOwner, Observer { movies ->
-            mViewModel.showLoading(false)
-            val mMovieAdapter =
-                MovieAdapter(this)
-            mMovieAdapter.swapData(movies)
-            binding.rvMovies.adapter = mMovieAdapter
-
-//            showResult()
-            mViewModel.cleanError()
-        })
-    }
-
-//    private fun setupLoadingObserver() {
-//        mViewModel.loading.observe(viewLifecycleOwner, Observer { status ->
-//            if (status == true) {
-//                binding.rvMovies.visibility = View.GONE
-//            } else {
-//                hideLoading()
-//            }
-//        })
-//    }
-//
-//    private fun setupErrorObserver() {
-//        mViewModel.error.observe(viewLifecycleOwner, Observer { error ->
-//            mViewModel.showLoading(false)
-//            if (error != null) {
-//                if (error.code == NETWORK_ERROR_CODE) {
-//                    showNoConnection(error.message)
-//                    tryAgain()
-//                } else {
-//                    showGenericError(error.message)
-//                }
-//            }
-//        })
-//    }
-
-//    private fun hideLoading() {
-//        binding.iBaseLayout.pbBase.visibility = View.GONE
-//    }
-//
-//    private fun showResult() {
-//        binding.iBaseLayout.groupNoConnection.visibility = View.GONE
-//        binding.rvMovies.visibility = View.VISIBLE
-//    }
-//
-//    private fun showNoConnection(message: String) {
-//        binding.rvMovies.visibility = View.GONE
-//        binding.iBaseLayout.tvNoConection.text = message
-//        binding.iBaseLayout.groupNoConnection.visibility = View.VISIBLE
-//    }
-//
-//    private fun showGenericError(message: String) {
-//        val sortDialog = AlertDialog.Builder(context)
-//            .setTitle(GENERIC_MSG_ERROR_TITLE)
-//            .setMessage(message)
-//            .setPositiveButton(R.string.dialog_ok, null)
-//            .create()
-//
-//        sortDialog.show()
-//    }
-//
-//    private fun tryAgain() {
-//        binding.iBaseLayout.btTryAgain.setOnClickListener { mViewModel.tryAgain() }
-//    }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -149,45 +77,6 @@ class MovieFragment : Fragment(), MovieClickListener {
         binding.viewModel = mViewModel
         setupObservers()
         return binding.root
-    }
-
-
-    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-        inflater.inflate(R.menu.menu_sort, menu)
-        super.onCreateOptionsMenu(menu, inflater)
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if (item.itemId == R.id.m_sort) {
-            val values = arrayOf<CharSequence>("Most Popular", "Top Rated", "Favorites")
-            val sortDialog = AlertDialog.Builder(context)
-                .setTitle(TITLE_DIALOG_FILTER)
-                .setSingleChoiceItems(values, mViewModel.selectedFilterIndex) { dialog, which ->
-                    changeSortOrder(which)
-                    dialog.dismiss()
-                }
-                .create()
-
-            sortDialog.show()
-        }
-        return super.onOptionsItemSelected(item)
-    }
-
-    private fun changeSortOrder(item: Int) {
-        when (item) {
-            INDEX_FILTER_MOST_POPULAR -> {
-                mViewModel.setMovieOrder(MovieOrderType.MostPopular)
-                mViewModel.selectedFilterIndex = 0
-            }
-            INDEX_FILTER_TOP_RATED -> {
-                mViewModel.setMovieOrder(MovieOrderType.TopHated)
-                mViewModel.selectedFilterIndex = 1
-            }
-            INDEX_FILTER_FAVORITES -> {
-                mViewModel.setMovieOrder(MovieOrderType.Favorites)
-                mViewModel.selectedFilterIndex = 2
-            }
-        }
     }
 
     override fun onMovieClick(movie: Movie) {
