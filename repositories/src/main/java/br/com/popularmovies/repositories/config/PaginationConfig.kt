839a7ccf29ebfddc6bf607d4d3aa5e2277981084
André Spadetto Leão <aleao@blissapplications.com>
2022-01-25 13:20:12 +0000

    Removing NowPlayingRemoteMediator and adjust pageSize config

--- repositories/src/main/java/br/com/popularmovies/repositories/config/PaginationConfig.kt
+++ repositories/src/main/java/br/com/popularmovies/repositories/config/PaginationConfig.kt
@@ -2,6 +2,6 @@ package br.com.popularmovies.repositories.config
 
 object PaginationConfig {
     const val defaultPage = 1
-    const val pageSize = 30
-    const val prefechDistance = 10
+    const val pageSize = 10
+    const val prefechDistance = 5
 }
