66fbd9f75a7523f18c0d6c11f0df5525771aec18
Andre Spadetto Leão <aleao@blissapplications.com>
2023-08-23 15:15:54 +0100

    Fix MovieTrailers component

--- feature/movie_details/src/main/java/br/com/popularmovies/moviedetails/ui/trailers/ui/MovieTrailers.kt
+++ feature/movie_details/src/main/java/br/com/popularmovies/moviedetails/ui/trailers/ui/MovieTrailers.kt
@@ -4,41 +4,44 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.PlayCircleOutline
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import br.com.popularmovies.core.designsystem.AppTheme
-import br.com.popularmovies.core.ui.R
 import br.com.popularmovies.model.movie.MovieTrailer
 
 @Composable
 fun MovieTrailerCard(movieTrailer: MovieTrailer, onClick: () -> Unit) {
     Card(
         shape = MaterialTheme.shapes.small,
-//        elevation = 2.dp,
         modifier = Modifier
             .width(200.dp)
             .height(150.dp)
             .clickable(onClick = onClick)
     ) {
         Column(
+            modifier = Modifier.fillMaxSize(),
             verticalArrangement = Arrangement.Center
         ) {
             Image(
-                painter = painterResource(R.drawable.ic_play_circle_outline),
+                imageVector = Icons.Filled.PlayCircleOutline,
+                colorFilter = ColorFilter.tint(color = MaterialTheme.colorScheme.onBackground),
                 contentDescription = null,
                 modifier = Modifier
                     .size(75.dp, 75.dp)
@@ -52,7 +55,7 @@ fun MovieTrailerCard(movieTrailer: MovieTrailer, onClick: () -> Unit) {
                     .padding(16.dp),
                 textAlign = TextAlign.Center,
                 style = MaterialTheme.typography.titleMedium,
-                color = MaterialTheme.colorScheme.onPrimary,
+                color = MaterialTheme.colorScheme.onBackground,
                 fontWeight = FontWeight.W500,
                 overflow = TextOverflow.Ellipsis,
                 maxLines = 2
